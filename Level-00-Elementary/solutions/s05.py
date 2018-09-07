bf6a931ebe806bb738c4f8b699a1cb78bc26ff2b
LeoCHANG <zhangshichaochina@gmail.com>
2018-09-07 20:08:04 +0800

    s07

--- Level-00-Elementary/solutions/s05.py
+++ Level-00-Elementary/solutions/s05.py
@@ -3,16 +3,15 @@ def between_markers(text: str, begin: str, end: str) -> str:
         returns substring between two given markers
     """
     # your code here
-    try:
-        start_index = text.index(begin)
-    except:
-        pass
-    try:
-        end_index = text.index(end)
-    except:
-        pass
+    start_index = 0 if text.find(begin) == -1 else text.find(begin)
+    len_begin = 0 if text.find(begin) == -1 else len(begin)
 
-    return text[start_index + len(begin):end_index]
+    end_index = text.find(end)
+
+    if end_index == -1:
+        return text[start_index + len_begin:]
+    else:
+        return text[start_index + len_begin:end_index]
 
 
 if __name__ == '__main__':
@@ -24,6 +23,7 @@ if __name__ == '__main__':
     assert between_markers("<head><title>My new site</title></head>",
                            "<title>", "</title>") == "My new site", "HTML"
     assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+    print(between_markers('No [b]hi', '[b]', '[/b]'))
     assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
     assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
     assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
